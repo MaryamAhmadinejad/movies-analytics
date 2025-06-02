68d2d68b33225c09de8bd2a96728e53198347b36
maryam-ahmadinejad <maryam.ahmadinejad91@gmail.com>
2025-06-02 19:16:02 +0330

    🔧 update BASE_URL

--- etl/extract/consts.py
+++ etl/extract/consts.py
@@ -1,5 +1,5 @@
-TMDB_BASE_API_URL = 'https://api.themoviedb.org'
-TMDB_API_KEY = '55ca0fcf3ff93755cd8ca9b4e5f42cea'
-TMDB_GENRES_LIST_API_PATH_V3 = '/3/genre/movie/list'
+TMDB_BASE_API_URL_V3 = 'https://api.themoviedb.org/3'
+TMDB_API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+TMDB_GENRES_LIST_API_PATH = '/genre/movie/list'
 
 RAW_GENRES_DATA_PATH = 'data/raw/genres_list.json'
--- etl/extract/genres.py
+++ etl/extract/genres.py
@@ -1,36 +1,36 @@
 import json
 import requests
-from consts import TMDB_BASE_API_URL, TMDB_API_KEY, TMDB_GENRES_LIST_API_PATH_V3, RAW_GENRES_DATA_PATH
+from consts import TMDB_BASE_API_URL_V3, TMDB_API_KEY, TMDB_GENRES_LIST_API_PATH, RAW_GENRES_DATA_PATH
 
 
 def fetch_genres_from_api():
-    print("⏳ Fetching genres from TMDb API...")
-    API_URL = f'{TMDB_BASE_API_URL}{TMDB_GENRES_LIST_API_PATH_V3}'
+    print('⏳ Fetching genres from TMDb API...')
+    API_URL = f'{TMDB_BASE_API_URL_V3}{TMDB_GENRES_LIST_API_PATH}'
     response = requests.get(API_URL, params={
-        "api_key": TMDB_API_KEY,
-        "language": "en-US"
+        'api_key': TMDB_API_KEY,
+        'language': 'en-US'
     })
     response.raise_for_status()
-    data = response.json().get("genres", [])
+    data = response.json().get('genres', [])
     # save data
-    with open(RAW_GENRES_DATA_PATH, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2, separators=(", ", ": "))
-    print(f"✅ Saved {len(data)} genres to {RAW_GENRES_DATA_PATH}")
+    with open(RAW_GENRES_DATA_PATH, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2, separators=(', ', ': '))
+    print(f'✅ Saved {len(data)} genres to {RAW_GENRES_DATA_PATH}')
     return data
 
 
 def load_or_fetch_genres():
     try:
-        with open("data/raw/genres_list.json", "r", encoding="utf-8") as f:
+        with open('data/raw/genres_list.json', 'r', encoding='utf-8') as f:
             data = json.load(f)
         print('✅ Data Exist!')
         return data
     except (FileNotFoundError, json.JSONDecodeError) as error:
-        print(f"⚠️  Error reading existing file: {error}")
+        print(f'⚠️  Error reading existing file: {error}')
         data = fetch_genres_from_api()
         return data
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     genres = load_or_fetch_genres()
-    print(f"\n✅ Loaded {len(genres)} genres:\n{genres[:3]}\n")
+    print(f'\n✅ Loaded {len(genres)} genres:\n{genres[:3]}\n')
